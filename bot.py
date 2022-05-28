0b9d230c8100edd96094f99d15d885ef9b513151
AresDza <90967711+AresDza@users.noreply.github.com>
2022-05-28 09:21:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 from JDatabase import JsonDatabase
 import start
 
-permitidos = ["AresDza","diago8888","Guachineo","FriendXfriendss"]
+permitidos = ["AresDza","diago8888","Guachineo","FriendXfriendss","manzanatg"]
 
 def filtrar_text(update, context):
     text = update.message.text
